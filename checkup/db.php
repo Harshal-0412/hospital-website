74d50ed2656575eddb0a2f3b0d356d380ccdeafc
HARSHAL JAIN <111647188+Harshal-0412@users.noreply.github.com>
2023-07-19 10:03:41 +0530

    Add files via upload

---
+++ checkup/db.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hello";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+//check connection
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+
+
+/*if (!$conn)
+{
+die("connection failed: " . mysqli_connect_error());
+}*/
+
+?>
+
